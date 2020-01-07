389be3691a292b24090d271860d0e5d559d2fed3
U-Anthony-PC\Anthony <donaldsa18@up.edu>
2020-01-07 06:16:41 -0800

    Added fbs files and made the table work

---
+++ gui.py
@@ -0,0 +1,128 @@
+from fbs_runtime.application_context.PyQt5 import ApplicationContext
+from rgbfinder import RGBFinder
+from PyQt5.QtWidgets import QTableWidget, QWidget, QVBoxLayout, QLabel, QAbstractItemView, QHBoxLayout, \
+    QSlider, QGridLayout, QGroupBox, QCheckBox, QHeaderView, QPushButton, QProgressBar, QTableWidgetItem, QDialog, QDialogButtonBox
+from PyQt5.QtGui import QIcon, QPixmap, QImage, QBrush, QColor
+from PyQt5.QtCore import Qt, QThread, QTimer, QSettings
+from functools import partial
+
+
+class Window(QWidget):
+    def __init__(self):
+        super(Window, self).__init__()
+        self.setWindowTitle("Vindictus Dye Finder")
+        self.image_label = None
+        self.table = None
+        self.layout = None
+        self.rgb_finder = None
+
+        self.init_image()
+        self.init_table()
+        self.set_layout()
+        self.show()
+        self.setFixedSize(self.layout.sizeHint())
+
+    def set_layout(self):
+        settings_button_box = self.make_settings_button_box()
+        bot_box = self.make_bot_box()
+
+        self.layout = QVBoxLayout()
+        self.layout.setAlignment(Qt.AlignTop)
+        self.layout.addSpacing(-12)
+        self.layout.addLayout(settings_button_box)
+        self.layout.addWidget(bot_box)
+
+        self.setLayout(self.layout)
+
+    def make_bot_box(self):
+        bot_layout = QHBoxLayout()
+
+        bot_layout.addWidget(self.image_label)
+        bot_layout.addWidget(self.table)
+        bot_box = QGroupBox()
+        bot_box.setLayout(bot_layout)
+        return bot_box
+
+    def show_preferences(self):
+        print("pref")
+
+    def make_settings_button_box(self):
+        settings_button = QPushButton()
+        # Gear icon is from: https://iconscout.com/icon/gear-222
+        style_sheet = """
+        QPushButton {
+            qproperty-icon: url(" ");
+            qproperty-iconSize: 15px 15px;
+            border-image: url("resources/Gear.svg");
+            background-color: rgba(255, 255, 255, 0);
+        }
+
+        QPushButton:hover {
+            border-image: url("resources/SelectedGear.svg");
+        }"""
+        settings_button.setStyleSheet(style_sheet)
+        # settings_button.setStyleSheet("background-color: rgba(0, 0, 0, 255); font-size: 23px;")
+        settings_button.clicked.connect(self.show_preferences)
+        settings_button.setFixedWidth(30)
+        settings_button.setFixedHeight(30)
+
+        settings_button_hb = QHBoxLayout()
+        settings_button_hb.setAlignment(Qt.AlignRight)
+        settings_button_hb.addWidget(settings_button)
+        settings_button_hb.addSpacing(-11)
+        return settings_button_hb
+
+    def init_table(self):
+        self.table = QTableWidget(7, 6)
+        self.table.setHorizontalHeaderLabels(['Color', 'Name', 'Red', 'Green', 'Blue', 'Move Mouse'])
+        self.table.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        header = self.table.horizontalHeader()
+        for i in range(6):
+            header.setSectionResizeMode(i, QHeaderView.ResizeToContents)
+        header.setSectionResizeMode(1, QHeaderView.Stretch)
+        self.table.setFixedWidth(300)
+
+    def insert_colors(self,colors):
+        # ["black", 25, 25, 25, 765, 0, 0]
+        for i in range(len(colors)):
+            color = colors[i]
+            brush = QBrush(QColor(color[1],color[2],color[3],255))
+            colored_item = QTableWidgetItem()
+            colored_item.setBackground(brush)
+            self.table.setItem(i, 0, colored_item)
+            for j in range(1, 5):
+                self.table.setItem(i, j, QTableWidgetItem(str(colors[i][j-1])))
+            button = QPushButton("({},{})".format(color[5], color[6]))
+            button.clicked.connect(partial(self.move_mouse, color[5], color[6]))
+            layout = QHBoxLayout()
+            layout.addWidget(button)
+            layout.setAlignment(Qt.AlignTop)
+            layout.setContentsMargins(0, 0, 0, 0)
+            widget = QWidget()
+            widget.setLayout(layout)
+            self.table.setCellWidget(i, 5, widget)
+
+    def move_mouse(self, x, y):
+        print("moved mouse to {},{}".format(x, y))
+        self.rgb_finder.move_mouse(x,y)
+
+    def init_image(self):
+        self.image_label = QLabel()
+        image = QPixmap('screencap.bmp')
+        self.image_label.setPixmap(image)
+
+    def set_rgb_finder(self,rgb_finder):
+        self.rgb_finder = rgb_finder
+
+
+class App:
+    def run(self):
+        app = ApplicationContext()
+        window = Window()
+        rgb_finder = RGBFinder(window)
+        rgb_finder.run()
+        return app.app.exec_()
+
+if __name__ == "__main__":
+    app = App()
+    app.run()
--- rgbfinder.py
+++ rgbfinder.py
@@ -4,11 +4,10 @@ import win32gui
 import win32ui
 import win32api
 import numpy as np
-import threading
-from datetime import datetime
+
 
 class RGBFinder:
-	def __init__(self):
+	def __init__(self, gui=None):
 		self.top = 268
 		self.left = 649
 		self.w = 907 - 651  # 904-651
@@ -19,10 +18,10 @@ class RGBFinder:
 		self.vindictus_h = 0
 		self.colors = [["black", 25, 25, 25, 765, 0, 0], ["white", 230, 230, 230, 765, 0, 0],
 					   ["pink", 200, 110, 200, 765, 0, 0], ["red", 200, 15, 15, 765, 0, 0]]
-		self.locks = {}
-		for color in self.colors:
-			color_name = color[0]
-			self.locks[color_name] = threading.Lock()
+
+		self.gui = gui
+		if gui is not None:
+			gui.set_rgb_finder(self)
 
 	def int8(self, x):
 		if x < 0:
@@ -95,7 +94,26 @@ class RGBFinder:
 		#print(colors[i][0],"at(",colors[i][5],",",colors[i][6],")", image[colors[i][6],colors[i][5]])
 		#win32api.SetCursorPos((x+left+colors[i][5],y+top+colors[i][6]))
 
-		print("Done checking for colors")
+
+		if self.gui is not None:
+			self.gui.insert_colors(self.colors)
+		else:
+			print("Done checking for colors")
+
+	def move_mouse(self, pix_x, pix_y):
+		x = 0
+		y = 0
+		hwnd = win32gui.FindWindow(None, "Vindictus")
+		if not hwnd:
+			hwnd = win32gui.FindWindow(None, "Vindictus Dye Finder")
+			# move mouse to the image instead of the actual Vindictus window
+			x -= self.left - 29
+			y -= self.top - 75
+
+		rect = win32gui.GetWindowRect(hwnd)
+		x += rect[0]
+		y += rect[1]
+		win32api.SetCursorPos((x + self.left + pix_x, y + self.top + pix_y))
 
 
 if __name__ == '__main__':
